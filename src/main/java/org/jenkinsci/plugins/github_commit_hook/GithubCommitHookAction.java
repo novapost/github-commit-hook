125ff6d03bc64afdc17f928da9844eb0521e53f2
Florent <florent.pigout@novapost.fr>
2014-08-04 19:52:00 +0200

    EnvVars keys refactoring.

--- src/main/java/org/jenkinsci/plugins/github_commit_hook/GithubCommitHookAction.java
+++ src/main/java/org/jenkinsci/plugins/github_commit_hook/GithubCommitHookAction.java
@@ -10,8 +10,8 @@ import org.kohsuke.stapler.export.ExportedBean;
 @ExportedBean
 public class GithubCommitHookAction extends InvisibleAction implements EnvironmentContributingAction {
 
-    public static final String GITHUB_HEAD_REF = "GITHUB_HEAD_REF";
-    public static final String GITHUB_HEAD_COMMIT = "GITHUB_HEAD_COMMIT";
+    public static final String GIT_COMMIT_HOOK_REF = "GIT_COMMIT_HOOK_REF";
+    public static final String GIT_COMMIT_HOOK_COMMIT = "GIT_COMMIT_HOOK_COMMIT";
 
     public final String ref;
     public final String commit;
@@ -22,8 +22,8 @@ public class GithubCommitHookAction extends InvisibleAction implements Environme
     }
 
     public void buildEnvVars(AbstractBuild<?, ?> build, EnvVars env) {
-        env.put(GITHUB_HEAD_REF, ref);
-        env.put(GITHUB_HEAD_COMMIT, commit);
+        env.put(GIT_COMMIT_HOOK_REF, ref);
+        env.put(GIT_COMMIT_HOOK_COMMIT, commit);
     }
 
 }
