ebd0d1478ebee47d2017974fc65c50da2aba88f7
Florent <florent.pigout@novapost.fr>
2014-08-05 09:45:13 +0200

    Update RefTokenMacro evaluate method return common GitBranchTokenMacro value if build was not triggered with GitBranchTokenMacro.

--- src/main/java/org/jenkinsci/plugins/github_commit_hook/GithubCommitHookRefTokenMacro.java
+++ src/main/java/org/jenkinsci/plugins/github_commit_hook/GithubCommitHookRefTokenMacro.java
@@ -6,6 +6,9 @@ import hudson.Extension;
 import hudson.model.AbstractProject;
 import hudson.model.TaskListener;
 import hudson.model.AbstractBuild;
+import hudson.plugins.git.Branch;
+import hudson.plugins.git.Revision;
+import hudson.plugins.git.util.BuildData;
 
 import org.jenkinsci.plugins.tokenmacro.DataBoundTokenMacro;
 import org.jenkinsci.plugins.tokenmacro.MacroEvaluationException;
@@ -25,10 +28,28 @@ public class GithubCommitHookRefTokenMacro extends DataBoundTokenMacro {
         AbstractProject<?, ?> project = build.getProject();
         GithubCommitHookJobProperty jobProperty = project.getProperty(GithubCommitHookJobProperty.class);
 
-        if (jobProperty == null) {
+        // return our property by default because build was triggered by our
+        // hoook.
+        if (jobProperty != null) {
+            return format(jobProperty.getRef());
+        }
+
+        BuildData data = build.getAction(BuildData.class);
+        if (data == null) {
             return "";
         }
 
-        return jobProperty.getRef();
+        Revision lb = data.getLastBuiltRevision();
+        if (lb==null || lb.getBranches().isEmpty()) {
+            return "";
+        }
+
+        // return same value as GitBranchTokenMacro otherwise
+        Branch branch = lb.getBranches().iterator().next();
+        return format(branch.getName());
+    }
+
+    private String format(String branchName) {
+        return branchName.substring(branchName.lastIndexOf('/')+1);
     }
 }
